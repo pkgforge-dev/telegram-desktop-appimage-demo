2c731a311cb605b257b46443710937a2cd065699
Link4Electronics <link4electronics@gmail.com>
2026-03-14 21:44:06 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -4,16 +4,15 @@ set -eu
 
 ARCH=$(uname -m)
 
-#	gst-libav        \
-#	gst-plugins-base \
-#	gst-plugins-bad  \
-#	gst-plugins-good \
-#	gst-plugin-va    \
-
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
 	geoclue          \
+	gst-libav        \
+	gst-plugins-base \
+	gst-plugins-bad  \
+	gst-plugins-good \
+	gst-plugin-va    \
 	pipewire-audio   \
 	pipewire-jack    \
 	telegram-desktop \
@@ -22,23 +21,3 @@ pacman -Syu --noconfirm \
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
-
-#echo "Getting telegram binary..."
-#echo "---------------------------------------------------------------"
-#mkdir -p ./AppDir/bin
-#if [ "$ARCH" = "x86_64" ]; then
-#	TARBALL_LINK=$(wget --retry-connrefused --tries=30 \
-#		https://api.github.com/repos/telegramdesktop/tdesktop/releases/latest -O - \
-#		| sed 's/[()",{} ]/\n/g' | grep -o -m 1 'https.*releases.*tsetup.*tar.xz'
-#	)
-#	wget "$TARBALL_LINK" -O /tmp/telegram.tar.xz
-#	tar xvf /tmp/telegram.tar.xz
-#	rm -f /tmp/telegram.tar.xz
-#	mv -v ./Telegram/Telegram ./AppDir/bin
-#	echo "$TARBALL_LINK" | awk -F'/' '{print $(NF-1)}' > ~/version
-#else
-#	pacman -S --noconfirm telegram-desktop
-#	VERSION=$(pacman -Q telegram-desktop | awk '{print $2; exit}')
-#	echo "$VERSION" > ~/version
-#	mv -v /usr/bin/Telegram ./AppDir/bin
-#fi
